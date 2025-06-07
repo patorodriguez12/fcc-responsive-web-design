9f51e3d8e68e1723f0ad96f68957711881674b17
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-06-07 20:12:03 -0300

    Full course page - home first test design

--- Full course/src/Pages/Home/Home.jsx
+++ Full course/src/Pages/Home/Home.jsx
@@ -1,13 +1,36 @@
 export default function Home() {
   return (
-    <div class="flex justify-center h-[100vh] items-center">
-      <ul>
-        <li><a href="/chapters/1">Chapter 1</a></li>
-        <li><a href="/chapters/2">Chapter 2</a></li>
-        <li><a href="/chapters/3">Chapter 3</a></li>
-        <li><a href="/chapters/4">Chapter 4</a></li>
-        <li><a href="/chapters/5">Chapter 5</a></li>
-      </ul>
+    <div class="flex flex-col gap-2 justify-center h-[100vh] items-center bg-gray-700">
+      <a
+        href="/chapters/1"
+        class="p-4 bg-amber-400 rounded font-bold hover:scale-105 transition-all"
+      >
+        Chapter 1
+      </a>
+      <a
+        href="/chapters/2"
+        class="p-4 bg-amber-400 rounded font-bold hover:scale-105 transition-all"
+      >
+        Chapter 2
+      </a>
+      <a
+        href="/chapters/3"
+        class="p-4 bg-amber-400 rounded font-bold hover:scale-105 transition-all"
+      >
+        Chapter 3
+      </a>
+      <a
+        href="/chapters/4"
+        class="p-4 bg-amber-400 rounded font-bold hover:scale-105 transition-all"
+      >
+        Chapter 4
+      </a>
+      <a
+        href="/chapters/5"
+        class="p-4 bg-amber-400 rounded font-bold hover:scale-105 transition-all"
+      >
+        Chapter 5
+      </a>
     </div>
-  )
+  );
 }
