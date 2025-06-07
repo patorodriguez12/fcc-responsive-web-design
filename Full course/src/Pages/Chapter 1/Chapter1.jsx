53a3c1d2db5cbe1739d0e5babc59b34cd26c2346
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-06-07 20:37:15 -0300

    Full course page - testing how to structure this proyect 2

--- Full course/src/Pages/Chapter 1/Chapter1.jsx
+++ Full course/src/Pages/Chapter 1/Chapter1.jsx
@@ -3,10 +3,6 @@ export default function Chapter1() {
     <div class="flex flex-col gap-2 justify-center h-[100vh] items-center bg-gray-700">
       <a href="/" class="pb-2 pt-2 pr-4 pl-4 bg-amber-400 rounded font-bold hover:bg-amber-900 transition-all">Back</a>
       <h1>Chapter 1</h1>
-      <a href="1/cat-photo-app">Cat Photo App</a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
     </div>
   )
 }
--- Full course/src/main.jsx
+++ Full course/src/main.jsx
@@ -9,7 +9,6 @@ import Chapter3 from "./Pages/Chapter 3/Chapter3.jsx";
 import Chapter4 from "./Pages/Chapter 4/Chapter4.jsx";
 import Chapter5 from "./Pages/Chapter 5/Chapter5.jsx";
 import NotFound from "./Pages/Not Found/NotFound.jsx";
-import CatPhotoApp from "./Pages/Chapter 1/Chapters/1-Cat Photo App/CatPhotoApp.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -17,7 +16,6 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="chapters/1" element={<Chapter1 />} />
-        <Route path="chapters/1/cat-photo-app" element={<CatPhotoApp />} />
         <Route path="chapters/2" element={<Chapter2 />} />
         <Route path="chapters/3" element={<Chapter3 />} />
         <Route path="chapters/4" element={<Chapter4 />} />
