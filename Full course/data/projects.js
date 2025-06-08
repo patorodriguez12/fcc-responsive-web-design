85ee408fec350ba08cfb0a43005ed6c541261bf8
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-06-08 10:54:02 -0300

    Full course page - projects data file added, fixed some minor errors

---
+++ Full course/data/projects.js
@@ -0,0 +1,108 @@
+export const chapters = [
+  {
+    id: 1,
+    name: "Chapter 1",
+    projects: [
+      {
+        id: 1,
+        name: "Cat Photo App",
+      },
+      {
+        id: 2,
+        name: "Cafe Menu App",
+      },
+      {
+        id: 3,
+        name: "Registration Form",
+      },
+      {
+        id: 4,
+        name: "Survey Form (certification)",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Chapter 2",
+    projects: [
+      {
+        id: 1,
+        name: "Rothko Painting",
+      },
+      {
+        id: 2,
+        name: "Photo Gallery",
+      },
+      {
+        id: 3,
+        name: "Nutrition Label",
+      },
+      {
+        id: 4,
+        name: "Quiz",
+      },
+      {
+        id: 5,
+        name: "Tribute Page (certification)",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Chapter 3",
+    projects: [
+      {
+        id: 1,
+        name: "Balance Sheet",
+      },
+      {
+        id: 2,
+        name: "Cat Painting",
+      },
+      {
+        id: 3,
+        name: "Responsive Piano",
+      },
+      {
+        id: 4,
+        name: "Technical Doc Page (certification)",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Chapter 4",
+    projects: [
+      {
+        id: 1,
+        name: "City Skyline",
+      },
+      {
+        id: 2,
+        name: "Magazine",
+      },
+      {
+        id: 3,
+        name: "Product Landing Page (certification)",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Chapter 5",
+    projects: [
+      {
+        id: 1,
+        name: "Ferris Wheel",
+      },
+      {
+        id: 2,
+        name: "Flappy Penguin",
+      },
+      {
+        id: 3,
+        name: "Portfolio (certification)",
+      },
+    ],
+  },
+];
